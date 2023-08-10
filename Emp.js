cd704867dd06da4a8183033b20d6d164d5a2c583
kirthi007 <chowdharykirthi@gmail.com>
2023-08-10 04:27:01 -0700

    Using Playwright to convert to pdf instead of Puppeteer

--- Emp.js
+++ Emp.js
@@ -1,30 +1,25 @@
-const puppeteer = require("puppeteer");
+const { chromium } = require('playwright');
+
 (async () => {
-  // Create a browser instance
-  const browser = await puppeteer.launch();
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-  // Create a new page
-  const page = await browser.newPage();
+  // Replace 'http://example.com' with the URL of the HTML page you want to convert
+  await page.goto('https://www.geeksforgeeks.org/introduction-to-express/');
 
-  // Website URL to export as pdf
-  const website_url =
-    "https://www.geeksforgeeks.org/steps-to-create-an-express-js-application/?ref=lbp";
-
-  // Open URL in current page
-  await page.goto(website_url, { waitUntil: "networkidle0" });
-
-  //To reflect CSS used for screens instead of print
-  await page.emulateMediaType("screen");
-
-  // Downlaod the PDF
+  // Adjust the dimensions of the PDF page if needed
   const pdf = await page.pdf({
-    path: "result.pdf",
-    margin: { top: "100px", right: "50px", bottom: "100px", left: "50px" },
-    printBackground: true,
-    format: "A4",
-    landscape: true,
+    path: 'result2.pdf', // Change the output file name and path as desired
+    format: 'A4',      // Page format, e.g., 'A4', 'Letter', etc.
+    margin: {
+      top: '20px',
+      bottom: '20px',
+      left: '20px',
+      right: '20px',
+    },
+    landscape: true
   });
 
-  // Close the browser instance
   await browser.close();
 })();
