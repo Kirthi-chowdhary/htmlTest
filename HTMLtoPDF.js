769a96fae26f01bd4d6b0bc49a37f39a177522e4
kirthi007 <chowdharykirthi@gmail.com>
2023-08-11 12:45:38 -0700

    deviding the data into chunks

--- HTMLtoPDF.js
+++ HTMLtoPDF.js
@@ -3,9 +3,17 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 const striptags = require('striptags');
 
-
 const app = express();
 const port = 3000;
+
+// Helper function to split text into chunks of a specific length
+function splitTextIntoChunks(text, chunkLength) {
+    const chunks = [];
+    for (let i = 0; i < text.length; i += chunkLength) {
+        chunks.push(text.slice(i, i + chunkLength));
+    }
+    return chunks;
+}
 
 app.get('/get-body-content', async (req, res) => {
     const url = req.query.url;
@@ -23,7 +31,10 @@ app.get('/get-body-content', async (req, res) => {
         const bodyContent = $('body').html();
         const plainTextContent = striptags(bodyContent);
 
-        res.json({ plainTextContent });
+        // Divide the plain text into chunks of 1500 words
+        const chunkedText = splitTextIntoChunks(plainTextContent, 1500);
+
+        res.json({ chunks: chunkedText });
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ error: 'An error occurred' });
