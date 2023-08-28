9981be1ec4f8d46b0719f0c5ecddb45d20e547eb
kirthi007 <chowdharykirthi@gmail.com>
2023-08-28 08:20:17 -0700

    Deviding the docx file into smaller parts

--- Controller/DocUploader.Controller.js
+++ Controller/DocUploader.Controller.js
@@ -3,7 +3,7 @@ const docuploader = require('../Utils/DocUploader');
 exports.Uploader = async (req, res) => {
   try {
     const parts = await docuploader.Doc(req.file.buffer);
-    console.log(parts)
+   
     res.status(200).json({ parts });
   } catch (error) {
     console.log(error);
--- Utils/DocUploader.js
+++ Utils/DocUploader.js
@@ -1,4 +1,7 @@
 const mammoth = require('mammoth');
+const officegen = require('officegen');
+const fs = require('fs');
+const path = require('path'); // Import the path module
 
 exports.Doc = async (doc) => {
   if (!doc) {
@@ -14,17 +17,38 @@ exports.Doc = async (doc) => {
     // Split text into parts based on estimatedWordsPerPart
     const parts = splitTextIntoParts(text);
 
-    // Now each part in 'parts' should be approximately within the size limit
-    // while avoiding breaking words.
-      
-    return parts;
+    const outputPath = path.join(__dirname, '..', 'output'); // Construct the output path
+    if (!fs.existsSync(outputPath)) {
+      fs.mkdirSync(outputPath);
+    }
+
+    const generatedFiles = []; // Array to store generated file paths
+
+    parts.forEach((partText, index) => {
+      const docx = officegen('docx');
+
+      const pObj = docx.createP();
+      pObj.addText(`Part ${index + 1}`, { bold: true });
+
+      const contentP = docx.createP();
+      contentP.addText(partText);
+
+      const outputFilePath = path.join(outputPath, `part_${index + 1}.docx`);
+      const outputStream = fs.createWriteStream(outputFilePath);
+      docx.generate(outputStream);
+
+      generatedFiles.push(outputFilePath); // Store the generated file path
+    });
+
+    // Return the array of generated file paths
+    return generatedFiles;
   } catch (error) {
     throw error;
   }
 };
 
 function splitTextIntoParts(text) {
-  const estimatedWordsPerPart = 1500; // Adjust this value based on your experiment
+  const estimatedWordsPerPart = 15000;
   const words = text.split(' ');
 
   const parts = [];
@@ -41,6 +65,6 @@ function splitTextIntoParts(text) {
   if (currentPart) {
     parts.push(currentPart);
   }
- 
+
   return parts;
 }
